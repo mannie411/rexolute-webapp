f30a8f6859a25974e2b5bd6872f422ece9d73a93
mannie411 <dev.mannie411@gmail.com>
2025-04-25 07:13:22 +0100

    dev: minor refactor

---
+++ src/pages/home/about.tsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import Image from "next/image";
+
+import { useRouter } from "next/router";
+import { Head } from "@/components/shared";
+
+const Page = () => {
+  const router = useRouter();
+  return (
+    <Fragment>
+      <Head title="App" />
+
+      <main className="flex h-screen w-screen justify-center items-center">
+        <h1>About</h1>
+      </main>
+    </Fragment>
+  );
+};
+
+export default Page;
